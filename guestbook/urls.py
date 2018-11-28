199f50280e5e23a62b8c174154980c9c01241d52
Wuilliam <wuilliam104286@gmail.com>
2018-11-28 16:06:06 +0800

    新增專案網站基底範本

--- guestbook/urls.py
+++ guestbook/urls.py
@@ -1,21 +1,22 @@
 """guestbook URL Configuration
 
-The `urlpatterns` list routes URLs to views. For more information please see:
+`urlpatterns`列表將URL路由到視圖。 有關更多信息，請參閱：
     https://docs.djangoproject.com/en/2.1/topics/http/urls/
-Examples:
+例如：
 Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+    1. 導入:  from my_app import views
+    2. 新增URL到urlpatterns:  path('', views.home, name='home')
 Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+    1. 導入:  from other_app.views import Home
+    2. 新增URL到urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+    1. 導入 include() function: from django.urls import include, path
+    2. 新增URL到urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('web.urls')),s
 ]
